224d637152a4aba60c6f089fa5cd55817a2c04b5
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2023-09-08 21:10:32 -0300

    first commit

---
+++ utils.py
@@ -0,0 +1,33 @@
+# Important
+name = "Hugo"
+
+### Conditionals
+if 1 == 1: # writes a if block
+    print("")
+elif 1 == 2:
+    print("")
+else:
+    print("")
+
+### Loops
+for value in values: # writes a for block
+    print(value)
+
+while something_is_true: # writes a while loop
+    print("")
+
+### Functions
+def my_function(parameter_one, parameter_two): # declares a function
+    print("")
+
+len() # Gives the length of a string
+type() # Gives the type of a variable
+str() # Converts to string
+round() # Rounds integer or float
+input() # gets input from the user on terminal
+print(f"") # prints to the terminal
+
+# Dictionary
+dictionary = { "Hugo": 123 }
+dictionary['Hugo']
+dictionary['New Entry'] = 456
