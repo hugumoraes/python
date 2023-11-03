95546621f0ba0884bee314d374aadb0f0f4e3541
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2023-11-03 12:21:09 -0300

    Progress

--- utils.py
+++ utils.py
@@ -1,26 +1,27 @@
-# Important
+# Variables
 name = "Hugo"
+something_is_true = True
+values: list[int] = [1, 2, 3]
 
 ### Conditionals
 if 1 == 1: # writes a if block
-    print("")
+    pass
 elif 1 == 2:
-    print("")
+    pass
 else:
-    print("")
+    pass
 
 ### Loops
 for value in values: # writes a for block
-    print(value)
+    pass
 
 while something_is_true: # writes a while loop
-    print("")
+    pass
 
 ### Functions
 def my_function(parameter_one, parameter_two): # declares a function
     """This is a new function""" ## Docstring
-    print("")
-    return 0
+    pass
 
 # Dictionary
 dictionary = { "Hugo": 123 }
@@ -33,6 +34,11 @@ array = [1, 2, 3, 4, 5]
 # Tuples
 tuple = (1, 2, 3, 4, 5)
 
+# Array slicing
+sliced = array[1:3] # [2, 3]
+# Array reverse
+reversed = array[::-1] # [5, 4, 3, 2, 1]
+
 
 # Useful functions
 len() # Gives the length of a string
@@ -41,3 +47,19 @@ str() # Converts to string
 round() # Rounds integer or float
 input() # gets input from the user on terminal
 print(f"") # prints to the terminal
+
+# Classes
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        print("Grrr")
+
+# Sub classes
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def make_sound(self):
+        print("Woof")
